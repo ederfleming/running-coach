3511e4c9afdc0d120abecb24945c7d62379f09f6
Eder Fleming <ederfleming@gmail.com>
2026-08-20 17:19:47 -0300

    fix(ios): format 3km time input

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'running-coach-v18';
+const CACHE_NAME = 'running-coach-v21';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
